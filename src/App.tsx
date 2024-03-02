94dddc1aa1b4b134c7d36fa5519949ddd7019017
Stalloyde <stalloyde93@gmail.com>
2024-03-02 14:20:45 +0800

    Implement app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import { useState } from 'react';
+import gameImage from './assets/whereswaldo.jpg';
+import wizardImage from './assets/wizard.gif';
+import wallyImage from './assets/wally-standing.png';
+import odlawImage from './assets/odlaw.gif';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <body>
+      <section>
+        <h2>Characters</h2>
+        <div>
+          <img src={wizardImage} alt='wizard' />
+          Wizard
+        </div>
+        <div>
+          <img src={wallyImage} alt='wally' />
+          Wally
+        </div>
+        <div>
+          <img src={odlawImage} alt='odlaw' />
+          Odlow
+        </div>
+      </section>
+
+      <main>
+        <div>
+          *Insert timer here*
+          <img src={gameImage} alt='wheres-waldo'></img>
+        </div>
+      </main>
+    </body>
   );
 }
 
