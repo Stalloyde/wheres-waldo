3cf91e9cb8eabfbb0a0da5f08f00c531e2249ed2
Stalloyde <stalloyde93@gmail.com>
2024-03-08 12:21:55 +0800

    Implement ClickMenu - Send mouseposition & target Character to backend

--- src/ClickMenu.tsx
+++ src/ClickMenu.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './ClickMenu.css';
 
 function ClickMenu({ setClick, mousePosition }) {
@@ -7,21 +8,51 @@ function ClickMenu({ setClick, mousePosition }) {
     left: `${mousePosition.x + 0.5}%`,
   };
 
-  function handleClick() {
-    setClick(false);
+  async function handleClick(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    const targetCharacterName = e.target.textContent.trim();
+    formData.append('targetCharacterName', targetCharacterName);
+    formData.append('xCoordinate', mousePosition.x);
+    formData.append('yCoordinate', mousePosition.y);
+
+    try {
+      const response = await fetch(`http://localhost:3000/`, {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        console.error('Error');
+      } else {
+        const responseData = await response.json();
+        console.log(responseData);
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setClick(false);
+    }
   }
+
   return (
     <>
       <ul style={clickMenuStyle}>
         Who's this?
         <li>
-          <button onClick={handleClick}>Wizard</button>
+          <button onClick={handleClick} name='targetCharacterName'>
+            Wizard
+          </button>
         </li>
         <li>
-          <button onClick={handleClick}>Wally</button>
+          <button onClick={handleClick} name='targetCharacterName'>
+            Wally
+          </button>
         </li>
         <li>
-          <button onClick={handleClick}>Odlaw</button>
+          <button onClick={handleClick} name='targetCharacterName'>
+            Odlaw
+          </button>
         </li>
       </ul>
     </>
