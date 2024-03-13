0a7ff43b3ba7ea03f4ab59f4ea3731b5cfa0fe9c
Stalloyde <stalloyde93@gmail.com>
2024-03-13 23:22:22 +0800

    Add tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,6 +5,10 @@ import App from './App';
 import userEvent from '@testing-library/user-event';
 import gameImage from './assets/whereswaldo.jpg';
 import { act } from 'react-dom/test-utils';
+import wizardImage from './assets/wizard.gif';
+import waldoImage from './assets/waldo-standing.png';
+import odlawImage from './assets/odlaw.gif';
+import ClickMenu from './ClickMenu';
 
 describe('App', () => {
   it('renders page', () => {
@@ -52,7 +56,7 @@ describe('Click on game image', () => {
     expect(setClick).toHaveBeenCalledWith(true);
   });
 
-  it('Renders ClickMenu on image click', async () => {
+  it('Clicking on image click toggles ClickMenu', async () => {
     const user = userEvent.setup();
     render(<App />);
 
@@ -60,25 +64,334 @@ describe('Click on game image', () => {
     await act(async () => user.click(gameImage));
     expect(screen.getByText(`Who's this?`)).toBeInTheDocument();
     expect(screen.getByAltText(`target`)).toBeInTheDocument();
+
+    await act(async () => user.click(gameImage));
+    expect(screen.queryByText(`Who's this?`)).not.toBeInTheDocument();
+    expect(screen.queryByAltText(`target`)).not.toBeInTheDocument();
   });
 });
 
-describe('Click on ClickMenu', () => {
-  it('Unrenders ClickMenu on Wizard button click', async () => {
+describe('Picking wrong characters unrenders ClickMenu and renders wrongCharacterModal ', () => {
+  beforeEach(async () => {
     const user = userEvent.setup();
     render(<App />);
-
     const gameImage = screen.getByAltText('wheres-waldo');
     await act(async () => user.click(gameImage));
-    expect(screen.getByText(`Who's this?`)).toBeInTheDocument();
-    expect(screen.getByAltText(`target`)).toBeInTheDocument();
+  });
 
+  it('Wizard ', async () => {
+    const user = userEvent.setup();
     const button = screen.getByRole('button', { name: 'Wizard' });
     await act(async () => user.click(button));
 
     await waitFor(() => {
       expect(screen.queryByText(`Who's this?`)).not.toBeInTheDocument();
       expect(screen.queryByAltText(`target`)).not.toBeInTheDocument();
+      expect(
+        screen.getByText(`Wrong Character... Try again.`),
+      ).toBeInTheDocument();
     });
+  });
+
+  it('Waldo', async () => {
+    const user = userEvent.setup();
+    const button = screen.getByRole('button', { name: 'Waldo' });
+    await act(async () => user.click(button));
+
+    await waitFor(() => {
+      expect(screen.queryByText(`Who's this?`)).not.toBeInTheDocument();
+      expect(screen.queryByAltText(`target`)).not.toBeInTheDocument();
+      expect(
+        screen.getByText(`Wrong Character... Try again.`),
+      ).toBeInTheDocument();
+    });
+  });
+
+  it('Odlaw', async () => {
+    const user = userEvent.setup();
+    const button = screen.getByRole('button', { name: 'Odlaw' });
+    await act(async () => user.click(button));
+
+    await waitFor(() => {
+      expect(screen.queryByText(`Who's this?`)).not.toBeInTheDocument();
+      expect(screen.queryByAltText(`target`)).not.toBeInTheDocument();
+      expect(
+        screen.getByText(`Wrong Character... Try again.`),
+      ).toBeInTheDocument();
+    });
+  });
+});
+
+describe('wrongCharacterModal closes', () => {
+  it('modal closes when clicking on gameImage', async () => {
+    const user = userEvent.setup();
+    render(<App />);
+
+    const gameImage = screen.getByAltText('wheres-waldo');
+    await act(async () => user.click(gameImage));
+
+    const button = screen.getByRole('button', { name: 'Odlaw' });
+    await act(async () => user.click(button));
+    await waitFor(() => {
+      expect(
+        screen.queryByText(`Wrong Character... Try again.`),
+      ).toBeInTheDocument();
+    });
+
+    await act(async () => user.click(gameImage));
+    await waitFor(() => {
+      expect(
+        screen.queryByText(`Wrong Character... Try again.`),
+      ).not.toBeInTheDocument();
+    });
+  });
+});
+
+describe('Picking a correct character updates its found status correctly', () => {
+  const mockCharactersNotFound = [
+    {
+      name: 'Wizard',
+      found: false,
+      src: wizardImage,
+      alt: 'Wizard',
+    },
+    {
+      name: 'Waldo',
+      found: false,
+      src: waldoImage,
+      alt: 'Waldo',
+    },
+    {
+      name: 'Odlaw',
+      found: false,
+      src: odlawImage,
+      alt: 'Odlaw',
+    },
+  ];
+
+  const setCharacters = vi.fn();
+  const handleClickMock = vi.fn();
+  const setMousePosition = vi.fn();
+
+  vi.spyOn(React, 'useState').mockImplementationOnce((initState) => [
+    initState,
+    setCharacters,
+  ]);
+
+  vi.spyOn(React, 'useState').mockImplementationOnce((initState) => [
+    initState,
+    setMousePosition,
+  ]);
+
+  beforeEach(() => {
+    handleClickMock.mockImplementationOnce((characterName) => {
+      const updatedCharacters = mockCharactersNotFound.map((prevState) => {
+        if (prevState.name === characterName)
+          return {
+            name: prevState.name,
+            found: true,
+            src: prevState.src,
+            alt: prevState.alt,
+          };
+
+        return prevState;
+      });
+      setCharacters(updatedCharacters);
+    });
+  });
+
+  it('Wizard', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <>
+        <ul>
+          Who's this?
+          {mockCharactersNotFound.map((character) => {
+            if (!character.found)
+              return (
+                <li key={character.name}>
+                  <button onClick={() => handleClickMock(character.name)}>
+                    {character.name}
+                  </button>
+                </li>
+              );
+          })}
+        </ul>
+      </>,
+    );
+    const button = screen.getByRole('button', { name: 'Wizard' });
+    await act(async () => user.click(button));
+
+    await waitFor(() => {
+      expect(setCharacters).toHaveBeenCalledWith([
+        {
+          name: 'Wizard',
+          found: true,
+          src: wizardImage,
+          alt: 'Wizard',
+        },
+        {
+          name: 'Waldo',
+          found: false,
+          src: waldoImage,
+          alt: 'Waldo',
+        },
+        {
+          name: 'Odlaw',
+          found: false,
+          src: odlawImage,
+          alt: 'Odlaw',
+        },
+      ]);
+    });
+  });
+
+  it('Waldo', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <>
+        <ul>
+          Who's this?
+          {mockCharactersNotFound.map((character) => {
+            if (!character.found)
+              return (
+                <li key={character.name}>
+                  <button onClick={() => handleClickMock(character.name)}>
+                    {character.name}
+                  </button>
+                </li>
+              );
+          })}
+        </ul>
+      </>,
+    );
+
+    const button = screen.getByRole('button', { name: 'Waldo' });
+    await act(async () => user.click(button));
+
+    await waitFor(() => {
+      expect(setCharacters).toHaveBeenCalledWith([
+        {
+          name: 'Wizard',
+          found: false,
+          src: wizardImage,
+          alt: 'Wizard',
+        },
+        {
+          name: 'Waldo',
+          found: true,
+          src: waldoImage,
+          alt: 'Waldo',
+        },
+        {
+          name: 'Odlaw',
+          found: false,
+          src: odlawImage,
+          alt: 'Odlaw',
+        },
+      ]);
+    });
+  });
+
+  it('Odlaw', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <>
+        <ul>
+          Who's this?
+          {mockCharactersNotFound.map((character) => {
+            if (!character.found)
+              return (
+                <li key={character.name}>
+                  <button onClick={() => handleClickMock(character.name)}>
+                    {character.name}
+                  </button>
+                </li>
+              );
+          })}
+        </ul>
+      </>,
+    );
+
+    const button = screen.getByRole('button', { name: 'Odlaw' });
+    await act(async () => user.click(button));
+
+    await waitFor(() => {
+      expect(setCharacters).toHaveBeenCalledWith([
+        {
+          name: 'Wizard',
+          found: false,
+          src: wizardImage,
+          alt: 'Wizard',
+        },
+        {
+          name: 'Waldo',
+          found: false,
+          src: waldoImage,
+          alt: 'Waldo',
+        },
+        {
+          name: 'Odlaw',
+          found: true,
+          src: odlawImage,
+          alt: 'Odlaw',
+        },
+      ]);
+    });
+  });
+});
+
+describe('Picking a correct character prevent its button from rendering in ClickMenu', () => {
+  const mockCharactersFound = [
+    {
+      name: 'Wizard',
+      found: true,
+      src: wizardImage,
+      alt: 'Wizard',
+    },
+    {
+      name: 'Waldo',
+      found: true,
+      src: waldoImage,
+      alt: 'Waldo',
+    },
+    {
+      name: 'Odlaw',
+      found: true,
+      src: odlawImage,
+      alt: 'Odlaw',
+    },
+  ];
+
+  it('No buttons are rendered', async () => {
+    render(
+      <>
+        <ul>
+          Who's this?
+          {mockCharactersFound.map((character) => {
+            if (!character.found)
+              return (
+                <li key={character.name}>
+                  <button onClick={() => handleClickMock(character.name)}>
+                    {character.name}
+                  </button>
+                </li>
+              );
+          })}
+        </ul>
+      </>,
+    );
+    expect(
+      screen.queryByRole('button', { name: 'Wizard' }),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole('button', { name: 'Waldo' }),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole('button', { name: 'Odlaw' }),
+    ).not.toBeInTheDocument();
   });
 });
